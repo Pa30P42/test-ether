18e7e87bba09b67a82efe272993aa706ad202464
Pa30p42 <hajiev93@gmail.com>
2020-10-22 23:55:42 +0300

    Updates

---
+++ precache-manifest.e86cd4ca0d6f298a7fbd5e5dd3e8e5f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d49102ca0b2962300b02a37807e761b",
+    "url": "/test-ether/index.html"
+  },
+  {
+    "revision": "2c82c3d383d725a3e300",
+    "url": "/test-ether/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "22dc4ea892d1b5f2f249",
+    "url": "/test-ether/static/css/main.75682884.chunk.css"
+  },
+  {
+    "revision": "2c82c3d383d725a3e300",
+    "url": "/test-ether/static/js/2.86e23cf7.chunk.js"
+  },
+  {
+    "revision": "179fe3fc0e78b5c2b8e2be1fa47bcaa6",
+    "url": "/test-ether/static/js/2.86e23cf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22dc4ea892d1b5f2f249",
+    "url": "/test-ether/static/js/main.fa4f1483.chunk.js"
+  },
+  {
+    "revision": "0edaa1f3246615c6e54d",
+    "url": "/test-ether/static/js/runtime-main.78ff5a57.js"
+  }
+]);
